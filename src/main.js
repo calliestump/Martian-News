f6e84de56a048004d0fba172430256895e98712b
David Sterry <sterry.david@gmail.com>
2020-11-18 14:06:43 -0800

    add API call, display divs, UI
    Co-authored-by: Callie Stump <callie@stu.mp>

--- src/main.js
+++ src/main.js
@@ -2,5 +2,16 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import Mars from '.js/mars.js'
 
-$
+$('#weather').submit(function () {
+  event.preventDefault();
+  let promise = Mars.getWeather();
+
+  promise.then(function (response) {
+    const body = JSON.parse(response);
+    $("#highTemp").text(`High Temp ${body.698.AT.mx}`);
+    $("#lowTemp").text(`Low Temp ${body.698.AT.mn}`);
+    $("#windSpeed").text(`Wind Speed ${body.698.HWS.av}`);
+  });
+});
