43941a6059043445f3f9e3cbe992f37575db620a
David Sterry <sterry.david@gmail.com>
2020-11-19 13:18:55 -0800

    add error messages to return to user
    Co-authored-by: Callie Stump <callie@stu.mp>

--- src/main.js
+++ src/main.js
@@ -11,7 +11,8 @@ $('#weather').click(function () {
   promise.then(function (resolvedResponse) {
     const body = JSON.parse(resolvedResponse);
     $('.marsWeather').text(`The high temp is ${body[698].AT.mx}°C and the low temp is ${body[698].AT.mn}°C with an average windspeed of ${body[698].HWS.av} kph`);
-  }, function(rejectedResponse) {
+  }, function (rejectedResponse) {
+    $('.marsWeather').text(`There was an error. This is the error message: ${rejectedResponse}`);
     console.log("oh no, an error!");
     console.log(rejectedResponse);
   });
@@ -20,22 +21,25 @@ $('#weather').click(function () {
 $('#astronomy').click(function () {
   event.preventDefault();
   let promise2 = Mars.getPicture();
-  promise2.then(function(response) {
+  promise2.then(function (response) {
     const body = JSON.parse(response);
-    $('.astronomyOutput').html(`This photo was taken by ${body.copyright} about this image ${body.explanation} <a href=${body.url}>Astronomy image</a>`);
-  }, function(rejectedResponse) {
+    $('.astronomyOutput').html(`This photo was taken by- ${body.copyright}, Description- ${body.explanation} <a href=${body.url}>Astronomy image</a>`);
+  }, function (rejectedResponse) {
+    $('.astronomyOutput').text(`There was an error. This is the error message: ${rejectedResponse}`);
     console.log("oh no,an error!");
     console.log(rejectedResponse);
   });
-});  
+});
 
 $('#rover').click(function () {
   event.preventDefault();
-  let promise3 = Mars.getRover();
-  promise3.then(function(response) {
+  const date = $("#dateInput").val();
+  let promise3 = Mars.getRover(date);
+  promise3.then(function (response) {
     const body = JSON.parse(response);
-    $('.roverOutput').html(`<a href=${body.photos[0].img_src}>Todays Rover photo</a>`);
-  }, function(rejectedResponse) {
+    $('.roverOutput').html(`<a href=${body.photos[0].img_src}>${date} Rover photo</a>`);
+  }, function (rejectedResponse) {
+    $('.roverOutput').text(`There was an error. This is the error message: ${rejectedResponse}`);
     console.log("oh no,an error!");
     console.log(rejectedResponse);
   });
