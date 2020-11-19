6676099c5e923c965a5fd742ac5e7d47cb9c60ab
David Sterry <sterry.david@gmail.com>
2020-11-18 16:24:22 -0800

    fix photo output, begin rover pic API

--- src/main.js
+++ src/main.js
@@ -7,8 +7,15 @@ import Mars from './mars.js';
 $('#weather').click(function () {
   event.preventDefault();
   let promise = Mars.getWeather();
+  let promise2 = Mars.getPicture();
+
   promise.then(function (response) {
     const body = JSON.parse(response);
-    $('.marsWeather').text(`The high temp is ${body[698].AT.mx}C and the low temp is ${body[698].AT.mn}C and the average windspeed is ${body[698].HWS.av} kph`);
+    $('.marsWeather').text(`<p>The high temp is</p> ${body[698].AT.mx}C and the low temp is ${body[698].AT.mn}C and the average windspeed is ${body[698].HWS.av} kph`);
+  });
+
+  promise2.then(function(response) {
+    const body = JSON.parse(response);
+    $('.astronomyOutput').html(`This photo was taken by ${body.copyright} about this image ${body.explanation} <a href=${body.url}>click here for image</a>`);
   });
 });
--- src/mars.js
+++ src/mars.js
@@ -15,4 +15,26 @@ export default class Mars {
       request.send();
     });
   }
+  static getPicture() {
+    return new Promise(function(resolve, reject) {
+      let request = new XMLHttpRequest();
+      const url = `https://api.nasa.gov/planetary/apod?api_key=${process.env.API_KEY}`;
+
+      request.onload = function () {
+        if (this.status === 200) {
+          resolve(request.response);
+        } else {
+          reject(request.response);
+        }
+      };
+      request.open("GET", url, true);
+      request.send();
+    });
+  }  
+  static getRover() {
+    return new Promise(function(resolve, reject) {
+      let request
+    })
+  }  
+  }
 }
