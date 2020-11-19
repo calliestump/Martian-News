92721b3e650db6b05d4d9db6abac4fc485439f12
David Sterry <sterry.david@gmail.com>
2020-11-19 10:08:09 -0800

    add new .click function, finish rover images
    Co-authored-by: Callie Stump <callie@stu.mp>

--- src/main.js
+++ src/main.js
@@ -7,15 +7,24 @@ import Mars from './mars.js';
 $('#weather').click(function () {
   event.preventDefault();
   let promise = Mars.getWeather();
-  let promise2 = Mars.getPicture();
-
   promise.then(function (response) {
     const body = JSON.parse(response);
     $('.marsWeather').text(`<p>The high temp is</p> ${body[698].AT.mx}C and the low temp is ${body[698].AT.mn}C and the average windspeed is ${body[698].HWS.av} kph`);
   });
-
+});
+$('#astronomy').click(function () {
+  event.preventDefault();
+  let promise2 = Mars.getPicture();
   promise2.then(function(response) {
     const body = JSON.parse(response);
-    $('.astronomyOutput').html(`This photo was taken by ${body.copyright} about this image ${body.explanation} <a href=${body.url}>click here for image</a>`);
+    $('.astronomyOutput').html(`This photo was taken by ${body.copyright} about this image ${body.explanation} <a href=${body.url}>Astronomy image</a>`);
+  });
+});  
+$('#rover').click(function () {
+  event.preventDefault();
+  let promise3 = Mars.getRover();
+  promise3.then(function(response) {
+    const body = JSON.parse(response);
+    $('.roverOutput').html(`<a href=${body.photos[0].img_src}>Todays Rover photo</a>`);
   });
 });
--- src/mars.js
+++ src/mars.js
@@ -33,8 +33,18 @@ export default class Mars {
   }  
   static getRover() {
     return new Promise(function(resolve, reject) {
-      let request
-    })
-  }  
+      let request = new XMLHttpRequest();
+      const url = `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?earth_date=2020-6-3&api_key=${process.env.API_KEY}`;
+
+      request.onload = function () {
+        if (this.status === 200) {
+          resolve(request.response);
+        } else {
+          reject(request.response);
+        }
+      };
+      request.open("GET", url, true);
+      request.send();
+    });  
   }
 }
